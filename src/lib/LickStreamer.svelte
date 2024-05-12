4dae6842ba9777829cac8c00b706be5c57dfb5d3
loaloaf <simon.steffens@live.de>
2024-05-12 15:17:17 +0200

    lick stream build

---
+++ src/lib/LickStreamer.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { portentaData } from "../../store/stores";
+  import PortentaStreamer from "./PortentaStreamer.svelte";
+</script>
+
+<PortentaStreamer
+  title={"Lick Sensor"}
+  dataStore={ portentaData }
+  wsEndpointName={"portentaoutput"}
+  minYData={0}
+  maxYData={256}
+>
+  <div slot="plotarea">
+  </div>
+</PortentaStreamer>
